1fb17c187cd99500845c5f3bf687d6cc4cff9442
mak <mak@lokalhost.pl>
2019-05-26 16:00:25 +0200

    read/write_memory for ghidra

--- src/backend/binja/__init__.py
+++ src/backend/binja/__init__.py
@@ -2,20 +2,37 @@ import binaryninja
 
 class _binja:
 
+	def view(self):
+		return bview
+
     def __getattr__(self, name):
-        return getattr(bapi,name)
+        return getattr(bview,name)
+
+
 
 binja = _binja()
-bapi = None
+bview = None
+
+get_function = lambda a: Func(a)
 
 def list_functions(**kwargs):
     return map(lambda f: Func(f.start),binja.functions)
 
+def read_bytes(addr, size):
+	br = binaryninja.BinaryReader(binja.view)
+	br.seek(addr)
+	return bytearray(br.read(size))
+
+def write_bytes(addr, bytes):
+	bw = binaryninja.BinaryWrite(bview)
+	bw.seek(addr)
+	bw.write(bytearray(bytes))
+
 def open(file):
-    global bapi
-    bapi = binaryninja.BinaryViewType.get_view_of_file(file)
-    bapi.add_analysis_option("linearsweep")
-    bapi.update_analysis_and_wait()
-    return bapi
+    global bview
+    bview = binaryninja.BinaryViewType.get_view_of_file(file)
+    bview.add_analysis_option("linearsweep")
+    bview.update_analysis_and_wait()
+    return bview
 
 from .objects import *
